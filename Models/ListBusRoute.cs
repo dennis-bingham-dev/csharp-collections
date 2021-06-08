f2077062838b9a62a6af13f00d6392d7ed672ad0
Dennis Bingham <dennis.bingham.dev@gmail.com>
2021-06-07 22:05:27 -0600

    fixed comment

--- Models/ListBusRoute.cs
+++ Models/ListBusRoute.cs
@@ -8,7 +8,7 @@ namespace collection_practice.Models
 
         public List<string> Stops { get; set;}
         public string Departure => Stops[0];
-        public string Arrival => Stops[^1]; // ^1 same as Stops.Length - 1
+        public string Arrival => Stops[^1]; // ^1 same as Stops.Count - 1
 
         public ListBusRoute(int route, List<string> stops) {
             this.Route = route;
